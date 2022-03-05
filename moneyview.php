e8f4b7ddc12b05ce63aa91c60b04e4ca0f089614
Riya-2807 <75556751+Riya-2807@users.noreply.github.com>
2022-03-05 14:49:24 +0530

    Add files via upload

---
+++ moneyview.php
@@ -0,0 +1,108 @@
+<?php
+    include "navbar.php";
+?>
+<br><br><br><br>
+<!doctype html>
+<html lang="en">
+<style>
+body {
+  background-image: url('images/contact.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+
+<head>
+
+    <meta charset="utf-8">
+	<title>GRIP Bank</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<link rel="stylesheet" href="style2.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+  <class >
+          <h2 style="text-align:center;">
+               <strong >All Users</strong>
+          </h2>
+      </class>
+    <?php
+    include 'connect.php';
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+    ?>
+
+
+
+
+
+
+    </div>
+
+
+    <div class="container">
+        <div class="row text-center">
+            <div class="col text-center">
+                <div class="table-responsive-sm">
+                    <table class="table table-hover table-striped table-sm">
+                        <thead style="color : black;" class="table-danger">
+                            <tr>
+                                <th scope="col" class="text-center py-2">S.No.</th>
+                                <th scope="col" class="text-center py-2">Name</th>
+                                <th scope="col" class="text-center py-2">E-Mail</th>
+                                <th scope="col" class="text-center py-2">Balance</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($rows = mysqli_fetch_assoc($result)) {
+                            ?>
+                                <tr style="color : black;">
+                                    <td class="text-center py-2">
+                                        <?php echo $rows['id'] ?>
+                                    </td>
+                                    <td class="text-center py-2">
+                                        <?php echo $rows['name'] ?>
+                                    </td>
+                                    <td class="text-center py-2">
+                                        <?php echo $rows['email'] ?>
+                                    </td>
+                                    <td class="text-center py-2">
+                                        <?php echo $rows['balance'] ?>
+                                    </td>
+
+                                </tr>
+                            <?php
+                            }
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="foot">
+        <footer class="bg-light text-center text-lg-start">
+
+            <div class="text-center p-3" style="background-color: grey;text-align:center;color:white;">
+                © 2022 Copyright - Made by <strong>Riya Ranglani</strong> :
+                <a class="text-dark" href="https://www.thesparksfoundationsingapore.org/" target="_blank" style="color:white;"> The Sparks
+                    Foundation </a>
+            </div>
+
+        </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="TemplateMo">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+  <title>TSF Bank</title>
+
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+  <link rel="stylesheet" href="assets/css/templatemo-lava.css">
+
+  <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+</head>
+
+<header class="header-area header-sticky">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <nav class="main-nav" style="color:white">
+          <a href="index.html" class="logo">
+            TSF Bank
+          </a>
+          <ul class="nav">
+            <li><a href="index.php" class="menu-item">Home</a></li>
+            <li><a href="moneyview.php" class="menu-item">View Users</a></li>
+            <li><a href="money.php" class="menu-item">Transfer Credit</a></li>
+            <li><a href="history.php" class="menu-item">Transaction History</a></li>
+          </ul>
+          <a class='menu-trigger'>
+            <span>Menu</span>
+          </a>
+
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
